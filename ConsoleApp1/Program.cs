303cfba9ac5e3814271c7adee289928f7f8ea668
serraguti <pacoserranox@gmail.com>
2026-06-09 20:44:57 +0200

    dsadsa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,78 +1,32 @@
-﻿using Newtonsoft.Json;
-using System.Text;
+﻿using OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
 
-const string API_KEY = "<Key>";
-const string ENDPOINT = "<URL>";
+const string deploymentName = "gpt-4.1";
+const string endpoint = "<endpoint>";
+const string apiKey = "<api-key>";
+
+ChatClient client = new(
+    credential: new ApiKeyCredential(apiKey),
+    model: deploymentName,
+    options: new OpenAIClientOptions()
+    {
+        Endpoint = new($"{endpoint}"),
+    });
 Console.WriteLine("Dime tu pregunta:");
 var question = Console.ReadLine();
-await AskQuestion(question);
 
-static async Task AskQuestion(string question)
+ChatCompletion completion = client.CompleteChat(
+     [
+         new SystemChatMessage("Eres un ayudante para un examen de programación"),
+         new UserChatMessage(question),
+     ]);
+
+Console.WriteLine($"Model={completion.Model}");
+foreach (ChatMessageContentPart contentPart in completion.Content)
 {
-    var payload = new
-    {
-        messages = new object[]
-        {
-                new {
-                    role = "system",
-                    content = new object[] {
-                        new {
-                            type = "text",
-                            text = "You are an AI assistant that helps people find information."
-                        }
-                    }
-                },
-                new {
-                    role = "user",
-                    content = new object[] {
-                        new {
-                            type = "text",
-                            text = question
-                        }
-                    }
-                }
-        },
-        temperature = 0.7,
-        top_p = 0.95,
-        max_tokens = 800,
-        stream = false
-    };
-    await SendRequest(payload);
-}
-
-static async Task SendRequest(object payload)
-{
-    using (var httpClient = new HttpClient())
-    {
-        httpClient.DefaultRequestHeaders.Add("api-key", API_KEY);
-
-        var response = await httpClient.PostAsync(ENDPOINT, new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"));
-
-        if (response.IsSuccessStatusCode)
-        {
-            dynamic jsonObject = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
-            var responseData = jsonObject?.choices?[0]?.message?.content;
-            var completionTokens = jsonObject?.usage.completion_tokens;
-            var prompt_tokens = jsonObject?.usage.prompt_tokens;
-            var total_tokens = jsonObject?.usage.total_tokens;
-
-            if (responseData != null)
-            {
-                Console.WriteLine(responseData);
-                Console.WriteLine("--------------------");
-                Console.WriteLine($"Completion tokens: {completionTokens}");
-                Console.WriteLine($"Prompt tokens: {prompt_tokens}");
-                Console.WriteLine($"Total tokens: {total_tokens}");
-                Console.WriteLine("--------------------");
-            }
-            else
-            {
-                Console.WriteLine("Response data is null.");
-            }
-        }
-        else
-        {
-            Console.WriteLine($"Error: {response.StatusCode}, {response.ReasonPhrase}");
-        }
-    }
+    string message = contentPart.Text;
+    Console.WriteLine($"Chat Role: {completion.Role}");
+    Console.WriteLine("Message:");
+    Console.WriteLine(message);
 }
